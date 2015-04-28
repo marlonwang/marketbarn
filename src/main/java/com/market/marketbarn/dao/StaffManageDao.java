6095186655f781b100eb846fb60346efa87e6e2a
marlonwang <marlonwang@163.com>
2015-04-28 21:38:50 +0800

    add new class to staff

--- src/main/java/com/market/marketbarn/dao/StaffManageDao.java
+++ src/main/java/com/market/marketbarn/dao/StaffManageDao.java
@@ -1,5 +1,24 @@
 package com.market.marketbarn.dao;
 
-public class StaffManageDao {
+import com.market.marketbarn.model.StaffInfo;
 
+public class StaffManageDao {
+	
+	// 进货员工
+	private StaffInfo stockStaff;
+	
+	// 出货员工
+	private StaffInfo salesStaff;
+	
+	// 查询进货人员
+	public StaffInfo getStockStaffById(int staffId){
+		
+		return stockStaff;
+	}
+	
+	// 查询出货人员
+	public StaffInfo getSalesStaffById(int staffId){
+		
+		return salesStaff;
+	}
 }
